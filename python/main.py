95b6fea779c19626933301039df23ff935ad5bf7
Qredo LTD <support@qredo.com>
2023-03-30 12:57:37 +0200

    modifying main

--- python/main.py
+++ python/main.py
@@ -14,29 +14,66 @@ def generate_test_data(num_data_points):
 
     return test_data
 
+def insert_classic(test_data):
+    balances = []
+    for balance, _ in test_data:
+        balances.append(balance)
+    return balances
+
+def sum_classic(balances):
+    total = 0
+    for balance in balances:
+        total += balance
+    return total
+
 def main():
     num_data_points = 1000
 
     test_data = generate_test_data(num_data_points)
 
+    # Classic insertion
+    start = timer()
+    balances = insert_classic(test_data)
+    end = timer()
+    classic_insert_time = end - start
+
+    # Classic sum
+    start = timer()
+    classic_sum = sum_classic(balances)
+    end = timer()
+    classic_sum_time = end - start
+
     tree = SelfBalancingBinaryTree()
 
-    # Insert test data into the tree
+    # AVL insertion
     start = timer()
     for balance, utxo in test_data:
         tree.insert(balance, utxo)
     end = timer()
-    insert_time = end - start
+    avl_insert_time = end - start
 
-    # Calculate the sum of balances
+    # AVL sum
     start = timer()
-    balance_sum = tree.sum_balance_to_root(tree.root)
+    avl_sum = tree.sum_balance_to_root(tree.root)
     end = timer()
-    sum_time = end - start
+    avl_sum_time = end - start
 
-    print(f"Insert time: {insert_time:.6f}s")
-    print(f"Sum time: {sum_time:.6f}s")
-    print(f"Sum of balances: {balance_sum}")
+    print("Classic Insertion:")
+    print(f"Insert time: {classic_insert_time:.6f}s")
+    print()
+
+    print("AVL Insertion:")
+    print(f"Insert time: {avl_insert_time:.6f}s")
+    print()
+
+    print("Classic Sum:")
+    print(f"Sum time: {classic_sum_time:.6f}s")
+    print(f"Sum of balances: {classic_sum}")
+    print()
+
+    print("AVL Sum:")
+    print(f"Sum time: {avl_sum_time:.6f}s")
+    print(f"Sum of balances: {avl_sum}")
 
 if __name__ == "__main__":
     main()
