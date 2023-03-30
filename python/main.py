96ae058dad9f75b704517a76d7728a485eab06f9
Qredo LTD <support@qredo.com>
2023-03-30 14:41:48 +0200

    adding AVLSum

--- python/main.py
+++ python/main.py
@@ -1,79 +1,90 @@
 from sbt import *
-from faker import Faker
-import random
+
 from time import time as timer
 
-def generate_test_data(num_data_points):
-    fake = Faker()
-    test_data = []
-
-    for _ in range(num_data_points):
-        balance = random.uniform(0.01, 1000)
-        utxo = fake.sha256()
-        test_data.append((balance, utxo))
-
-    return test_data
-
-def insert_classic(test_data):
-    balances = []
-    for balance, _ in test_data:
-        balances.append(balance)
-    return balances
-
-def sum_classic(balances):
-    total = 0
-    for balance in balances:
-        total += balance
-    return total
+import random
 
 def main():
-    num_data_points = 1000
+    num_data_points = 10000000
 
     test_data = generate_test_data(num_data_points)
 
-    # Classic insertion
+    # Classic insertion, deletion, modification, and sum
     start = timer()
     balances = insert_classic(test_data)
     end = timer()
     classic_insert_time = end - start
 
-    # Classic sum
     start = timer()
     classic_sum = sum_classic(balances)
     end = timer()
     classic_sum_time = end - start
 
-    tree = SelfBalancingBinaryTree()
+    sum_tree = AVLSumTree()
 
     # AVL insertion
     start = timer()
     for balance, utxo in test_data:
-        tree.insert(balance, utxo)
+        sum_tree.insert(balance, utxo)
     end = timer()
     avl_insert_time = end - start
 
-    # AVL sum
+    # AVL sum before
     start = timer()
-    avl_sum = tree.sum_balance_to_root(tree.root)
+    avl_sum_before = sum_tree.get_total_sum()
     end = timer()
-    avl_sum_time = end - start
+    avl_sum_time_before = end - start
 
-    print("Classic Insertion:")
+    # Choose a random index for deletion and modification
+    rand_index = random.randint(0, num_data_points - 1)
+
+    # AVL deletion
+    start = timer()
+    sum_tree.delete(test_data[rand_index][0], test_data[rand_index][1])
+    end = timer()
+    avl_delete_time = end - start
+
+    # AVL modification
+    start = timer()
+    sum_tree.modify(test_data[rand_index][0], test_data[rand_index][0] * 2, test_data[rand_index][1])
+    end = timer()
+    avl_modify_time = end - start
+
+    # AVL root sum
+    start = timer()
+    avl_sum = sum_tree.get_total_sum()
+    end = timer()
+    avl_sum_time_after = end - start
+
+    # Classic deletion
+    start = timer()
+    delete_classic(balances, test_data[rand_index][0])
+    end = timer()
+    classic_delete_time = end - start
+
+    # Classic modification
+    start = timer()
+    modify_classic(balances, test_data[rand_index][0], test_data[rand_index][0] * 2)
+    end = timer()
+    classic_modify_time = end - start
+
+    print("Classic Insertion and Sum:")
+    print(f"Sum of balances before: {classic_sum:.6f}s")
     print(f"Insert time: {classic_insert_time:.6f}s")
-    print()
-
-    print("AVL Insertion:")
-    print(f"Insert time: {avl_insert_time:.6f}s")
-    print()
-
-    print("Classic Sum:")
     print(f"Sum time: {classic_sum_time:.6f}s")
     print(f"Sum of balances: {classic_sum}")
+    print(f"Delete time: {classic_delete_time:.6f}s")
+    print(f"Modify time: {classic_modify_time:.6f}s")
     print()
 
-    print("AVL Sum:")
-    print(f"Sum time: {avl_sum_time:.6f}s")
-    print(f"Sum of balances: {avl_sum}")
+    print("AVL Insertion, Deletion, Modification, and Root Sum:")
+    print(f"Insert time: {avl_insert_time:.6f}s")
+    print(f"Sum of balances before: {avl_sum_before:.6f}s")
+    print(f"Sum time before: {avl_sum_time_before:.6f}s")
+    print(f"Delete time: {avl_delete_time:.6f}s")
+    print(f"Modify time: {avl_modify_time:.6f}s")
+    print(f"Sum time: {avl_sum_time_after:.6f}s")
+    print(f"Sum of balances after: {avl_sum}")
 
 if __name__ == "__main__":
     main()
--- python/sbt.py
+++ python/sbt.py
@@ -1,3 +1,6 @@
+from faker import Faker
+import random
+
 class Node:
     def __init__(self, balance, utxo):
         self.balance = balance
@@ -82,3 +85,122 @@ class SelfBalancingBinaryTree:
         if node is None:
             return 0
         return node.balance + self.sum_balance_to_root(node.left) + self.sum_balance_to_root(node.right)
+    
+class AVLSumTree(SelfBalancingBinaryTree):
+    def __init__(self):
+        super().__init__()
+        self.total_sum = 0
+
+    def insert(self, balance, utxo):
+        super().insert(balance, utxo)
+        self.total_sum += balance
+
+    def delete(self, balance, utxo):
+        node = self._search(self.root, balance, utxo)
+        if node:
+            self.total_sum -= balance
+            self.root = self._delete(self.root, balance, utxo)
+
+    def modify(self, old_balance, new_balance, utxo):
+        self.delete(old_balance, utxo)
+        self.insert(new_balance, utxo)
+
+    def get_total_sum(self):
+        return self.total_sum
+
+    def _search(self, node, balance, utxo):
+        if node is None:
+            return None
+
+        if balance == node.balance and utxo == node.utxo:
+            return node
+
+        if balance < node.balance:
+            return self._search(node.left, balance, utxo)
+
+        return self._search(node.right, balance, utxo)
+
+    def _delete(self, node, balance, utxo):
+        if node is None:
+            return node
+
+        if balance < node.balance:
+            node.left = self._delete(node.left, balance, utxo)
+        elif balance > node.balance:
+            node.right = self._delete(node.right, balance, utxo)
+        else:
+            if utxo == node.utxo:
+                if node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+
+                temp = self._find_min(node.right)
+                node.balance = temp.balance
+                node.utxo = temp.utxo
+                node.right = self._delete(node.right, temp.balance, temp.utxo)
+            else:
+                if node.left and utxo < node.left.utxo:
+                    node.left = self._delete(node.left, balance, utxo)
+                elif node.right and utxo > node.right.utxo:
+                    node.right = self._delete(node.right, balance, utxo)
+
+        self.update_height(node)
+        balance_factor = self.balance_factor(node)
+
+        if balance_factor > 1 and self.balance_factor(node.left) >= 0:
+            return self.rotate_right(node)
+
+        if balance_factor < -1 and self.balance_factor(node.right) <= 0:
+            return self.rotate_left(node)
+
+        if balance_factor > 1 and self.balance_factor(node.left) < 0:
+            node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
+
+        if balance_factor < -1 and self.balance_factor(node.right) > 0:
+            node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
+
+        return node
+
+    def _find_min(self, node):
+        if node.left is None:
+            return node
+        return self._find_min(node.left)
+
+def generate_test_data(num_data_points):
+    fake = Faker()
+    test_data = []
+
+    for _ in range(num_data_points):
+        balance = random.uniform(0.01, 1000)
+        utxo = fake.sha256()
+        test_data.append((balance, utxo))
+
+    return test_data
+
+def insert_classic(test_data):
+    balances = []
+    for balance, _ in test_data:
+        balances.append(balance)
+    return balances
+
+def sum_classic(balances):
+    total = 0
+    for balance in balances:
+        total += balance
+    return total
+
+def delete_classic(balances, balance):
+    for i, b in enumerate(balances):
+        if b == balance:
+            del balances[i]
+            break
+
+def modify_classic(balances, old_balance, new_balance):
+    for i, b in enumerate(balances):
+        if b == old_balance:
+            balances[i] = new_balance
+            break
+
